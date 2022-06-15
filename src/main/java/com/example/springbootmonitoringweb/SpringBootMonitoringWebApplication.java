7c4666a95a4c64de43173c87e1202273193db0b5
leeha0 <gkdud2837@gmail.com>
2022-06-15 22:51:47 +0900

    Spring Boot Web - Actuator Prometheus 메트릭 추가

---
+++ src/main/java/com/example/springbootmonitoringweb/SpringBootMonitoringWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmonitoringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMonitoringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMonitoringWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootmonitoringweb/SpringBootMonitoringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmonitoringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMonitoringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
